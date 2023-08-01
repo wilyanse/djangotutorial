0ccafeaf38831d2cf0ed7eac35db2bfb154e1bee
Wilyanse <willjose51@gmail.com>
2023-08-01 09:06:47 +0800

    Finish Part 4
    Finished Part 4 of the youtube video that details how to create HTML templates.

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response, name):
-    ls = ToDoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s <br> <br> <p>%s</p></h1>" % (ls.name, str(item.text)))
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls": ls})
 
+def home(response):
+    return render(response, "main/home.html", {"name": "test"})
