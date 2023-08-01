9c5f0651c25fb3f5ceb81fbb35bce05273e37470
Wilyanse <willjose51@gmail.com>
2023-08-01 10:03:09 +0800

    Finish Part 5
    Part 5 detailed creating forms that Django already has built-in as well.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
@@ -8,4 +9,19 @@ def index(response, id):
     return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
-    return render(response, "main/home.html", {"name": "test"})
+    return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
+
